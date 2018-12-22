2ff06f8e9e16885ae4ef45af7ac722fc030fd9fb
Julian Oster <julian.oster@outlook.de>
2018-12-22 13:29:34 +0100

    Rename UseBreadcrumbs to AddBreadcrumbs
    Align with the other ASP.NET Core extension methods. The pattern Use...
    is normally used in the Configure() method, not in the ConfigureServices()
    method.

--- src/BreadcrumbExtensions.cs
+++ src/BreadcrumbExtensions.cs
@@ -9,19 +9,19 @@ namespace SmartBreadcrumbs
     public static class BreadcrumbExtensions
     {
 
-        public static void UseBreadcrumbs(this IServiceCollection services, Assembly assembly)
+        public static void AddBreadcrumbs(this IServiceCollection services, Assembly assembly)
         {
-            UseBreadcrumbs(services, assembly, new BreadcrumbOptions());
+            AddBreadcrumbs(services, assembly, new BreadcrumbOptions());
         }
 
-        public static void UseBreadcrumbs(this IServiceCollection services, Assembly assembly, Action<BreadcrumbOptions> optionsSetter)
+        public static void AddBreadcrumbs(this IServiceCollection services, Assembly assembly, Action<BreadcrumbOptions> optionsSetter)
         {
             var options = new BreadcrumbOptions();
             optionsSetter.Invoke(options);
-            UseBreadcrumbs(services, assembly, options);
+            AddBreadcrumbs(services, assembly, options);
         }
 
-        private static void UseBreadcrumbs(IServiceCollection services, Assembly assembly, BreadcrumbOptions options)
+        private static void AddBreadcrumbs(IServiceCollection services, Assembly assembly, BreadcrumbOptions options)
         {
             var bm = new BreadcrumbsManager();
             bm.Initialize(assembly, options);
