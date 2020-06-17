c69af2425554f7b9a924186ef4209f5605cc84f2
samuele-churchill <50848566+samuele-churchill@users.noreply.github.com>
2020-06-17 12:54:35 -0400

    Added MvcRouteBreadcrumbNode to allow generation of urls using routes rather than controller and action

---
+++ src/Nodes/MvcRouteBreadcrumbNode.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+
+namespace SmartBreadcrumbs.Nodes
+{
+    public class MvcRouteBreadcrumbNode: BreadcrumbNode
+    {
+
+        #region Properties
+
+        public string RouteName { get; set; }
+
+
+        #endregion
+
+        internal MvcRouteBreadcrumbNode(string routeName, BreadcrumbAttribute attr) : base(attr)
+        {
+            RouteName = routeName;
+        }
+
+        public MvcRouteBreadcrumbNode(string routeName, string title, bool overwriteTitleOnExactMatch = false, string iconClasses = null, string areaName = null)
+            : base(title, overwriteTitleOnExactMatch, iconClasses, areaName)
+        {
+            RouteName = routeName;
+        }
+
+        #region Public Methods
+
+        public override string GetUrl(IUrlHelper urlHelper) => urlHelper.RouteUrl(RouteName, RouteValues);
+
+        #endregion
+    }
+}
